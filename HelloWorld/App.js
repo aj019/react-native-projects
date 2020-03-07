ed089844e7747b605ac73497f61b918a12e491d8
Anuj Gupta <junagupta19@gmail.com>
2020-03-07 13:32:19 +0530

    Initial Commit

---
+++ HelloWorld/App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = function() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default App;
